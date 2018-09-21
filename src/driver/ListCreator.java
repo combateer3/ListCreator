48ed864242ae2bfda7e5d0895769e4046705bcd4
scott <scottcorbat@yahoo.com>
2018-09-20 22:11:21 -0400

    assigned fxml objects to java variables and methods

--- src/driver/ListCreator.java
+++ src/driver/ListCreator.java
@@ -1,9 +1,21 @@
 package driver;
 
-public class ListCreator {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ListCreator extends Application {
 
     public static void main(String[] args) {
-
+        launch(args);
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/gui/maingui.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
 }
--- src/gui/GUIController.java
+++ src/gui/GUIController.java
@@ -1,9 +1,39 @@
 package gui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class GUIController {
 
+    @FXML
+    private TextArea textArea;
 
+    @FXML
+    private TextField listNameField;
+
+    @FXML
+    private TextField itemNameField;
+
+    @FXML
+    private TextField itemPriceField;
+
+    @FXML
+    private TextField itemLinkField;
+
+    @FXML
+    private void loadList() {
+
+    }
+
+    @FXML
+    private void addItem() {
+
+    }
+
+    @FXML
+    private void saveList() {
+
+    }
 
 }
